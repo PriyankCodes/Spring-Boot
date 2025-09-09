c0281b6eec6ccea7bd814bd085674490398849e4
Priyank Fichadiya <priyank.fichadiya@tssconsultancy.com>
2025-09-09 17:51:29 +0530

    Updates

--- Bank_Management_System/src/main/java/com/tss/bank/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tss.bank.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
